3059ff9c974d57e5be2da6b0be793156315dd3d8
agam <agamnasy28@gmail.com>
2025-03-07 08:30:33 +0700

    first commit

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVarible = "Muhammad Agam Nasywaan"
+
+function getMyIdentity() {
+    let localVariable = 2211104085
+
+    console.log(`Nama saya ${globalVarible} nim ${localVariable}`)
+}
+
+console.log(globalVarible)
+// console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable.js
@@ -0,0 +1,17 @@
+// VAR
+
+var name = "Muhammad Agam Nasywaan"
+const nim = 2211104085
+
+console.log(name)
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim)
+console.log(`Nama saya adalah ${name} dan nim saya adalah ${nim}`)
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+
+var name = "ayam"
+
+let fullName = "Muhammad Agam Nasywaan"
